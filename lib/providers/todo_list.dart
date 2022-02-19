a764f5497e0e7f7c73e7427a4cda1c1f0f3fdd34
KIM WOORAM <fnhero@kakao.com>
2022-02-20 00:08:23 +0900

    투두리스트

---
+++ lib/providers/todo_list.dart
@@ -0,0 +1,85 @@
+import 'package:equatable/equatable.dart';
+import 'package:flutter/material.dart';
+
+import '../models/todo_model.dart';
+
+class TodoListState extends Equatable {
+  final List<Todo> todos;
+  const TodoListState({
+    required this.todos,
+  });
+
+  factory TodoListState.initial() {
+    return TodoListState(todos: [
+      Todo(id: '1', desc: '할일1'),
+      Todo(id: '2', desc: '할일2'),
+      Todo(id: '3', desc: '할일3'),
+    ]);
+  }
+
+  @override
+  String toString() => 'TodoListState(todos: $todos)';
+
+  @override
+  List<Object> get props => [todos];
+
+  TodoListState copyWith({
+    List<Todo>? todos,
+  }) {
+    return TodoListState(
+      todos: todos ?? this.todos,
+    );
+  }
+}
+
+class TodoList with ChangeNotifier {
+  TodoListState _state = TodoListState.initial();
+  TodoListState get state => _state;
+
+  void addTodo(String todoDesc) {
+    final newTodo = Todo(desc: todoDesc);
+    final newTodos = [..._state.todos, newTodo];
+
+    _state = _state.copyWith(todos: newTodos);
+    notifyListeners();
+  }
+
+  void editTodo(String id, String todoDesc) {
+    final newTodos = _state.todos.map((Todo todo) {
+      if (todo.id == id) {
+        return Todo(
+          id: id,
+          desc: todoDesc,
+          completed: todo.completed,
+        );
+      }
+      return todo;
+    }).toList();
+
+    _state = _state.copyWith(todos: newTodos);
+    notifyListeners();
+  }
+
+  void toggleTodo(String id) {
+    final newTodos = _state.todos.map((Todo todo) {
+      if (todo.id == id) {
+        return Todo(
+          id: id,
+          desc: todo.desc,
+          completed: !todo.completed,
+        );
+      }
+      return todo;
+    }).toList();
+
+    _state = _state.copyWith(todos: newTodos);
+    notifyListeners();
+  }
+
+  void removeTodo(Todo todo) {
+    final newTodos = _state.todos.where((Todo t) => t.id != todo.id).toList();
+
+    _state = _state.copyWith(todos: newTodos);
+    notifyListeners();
+  }
+}
